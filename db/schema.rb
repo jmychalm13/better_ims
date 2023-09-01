b97edc89dae08b03dff89136d79341ed7a5af935
“Jessica <jmychalm13@gmail.com>
2023-08-31 20:50:09 -0500

    working user model

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_011223) do
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_011905) do
   enable_extension "plpgsql"
 
   create_table "daily_usages", force: :cascade do |t|
@@ -39,4 +38,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_01_011223) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
