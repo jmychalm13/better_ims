e19928eeacfb7fe41840055144cbad7d5bcc6ba3
“Jessica <jmychalm13@gmail.com>
2023-09-10 17:07:08 -0500

    changed update order model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,17 +38,22 @@ class OrdersController < ApplicationController
   def update
     order = Order.find(params[:id])
     date = DateTime.now()
-    
+
     order.update(
       # change this to be today's date
       date_received: date || order.date_received,
     )
     # use a model method .where to pull back all productOrders with that order_id match params.id
+    product_orders = ProductOrder.where(order_id: params[:id])
     # loop through productOrders that are returned and set the received quantity based on the params
+    product_orders.each do |product_order|
+      pp product_order
+      pp params[product_order.id.to_s]
+      product_order.update(
+        quantity_received: params[product_order.id.to_s]
+      )
+    end
         # this will be an hash where the key is the productOrder ID and value is quantity received
     render json: order.as_json
-    pp "Here I am !!!!!!!!!!!"
-    pp date
-    pp params
   end
 end
