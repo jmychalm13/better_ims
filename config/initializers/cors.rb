d19efc7520e82e68bb29321ebbec58c6a81050a7
“Jessica <jmychalm13@gmail.com>
2023-09-17 09:07:12 -0500

    changed cors in attempt to fix deployment problems

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173", "better-ims-frontend.onrender.com"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    origins 'localhost:5173', "http://localhost:5173/", "localhost:5173/", "better-ims-frontend.onrender.com/"
+    
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
