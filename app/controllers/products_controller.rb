d074863b6b699fd8e54507cca6f28cd4dfb88709
“Jessica <jmychalm13@gmail.com>
2023-09-19 12:15:21 -0500

    deploying

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
-    render :index
+    products = Product.all
+    render json: products.as_json
   end
 
   def create
